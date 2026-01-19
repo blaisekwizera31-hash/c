01ee3b5619ac8130efcad98b44694e65de9ec616
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-19 09:07:58 +0200

    COMMIT

--- string.c
+++ string.c
@@ -8,3 +8,12 @@ int main(){
     printf("Hello, %s!\n", name);
     return 0;
 }
+
+#include <stdio.h>
+int main(){
+	char name[30];
+	printf("Enter name: ");
+	gets(name);
+	puts(name);
+//	printf("\n");
+}
